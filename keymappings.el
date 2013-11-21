ab050922d52204217ceb7d09ae39e4c6fef95b49
Adam Groves <adam.groves@gmail.com>
2013-11-21 10:39:31 +0100

    Ensure that C-j is remapped in the scratch buffer too
    Also, I had C-k and C-j the wrong way around

--- keymappings.el
+++ keymappings.el
@@ -7,7 +7,15 @@
 (global-set-key (kbd "M-v") 'yank)
 (global-set-key (kbd "M-c") 'kill-ring-save)
 (global-set-key (kbd "M-u") 'undo-tree-visualize)
-(global-set-key (kbd "C-k") 'next-line)
-(global-set-key (kbd "C-j") 'previous-line)
+(global-set-key (kbd "C-c k") 'kill-line)
+(global-set-key (kbd "C-k") 'previous-line)
+(global-set-key (kbd "C-j") 'next-line)
+
+(defun me-add-lisp-keys ()
+    (local-set-key (kbd "C-j") 'next-line))
+
+(add-hook 'lisp-interaction-mode-hook 'me-add-lisp-keys)
+
+(global-set-key (kbd "\C-ch") help-map)
 (global-set-key (kbd "C-h") 'backward-char)
 (global-set-key (kbd "C-l") 'forward-char)
