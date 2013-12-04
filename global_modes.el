791c1a51ce012bce69970cd5581099ac58a5e61f
Adam Groves <adam.groves@gmail.com>
2013-12-04 11:20:22 +0100

    There was some problem with ido-completions
    This was causing the command line to be unusable. I've removed for now
    ido-vertical and ido-ubiquitous and it's working again.

--- global_modes.el
+++ global_modes.el
@@ -2,13 +2,9 @@
 (global-rainbow-delimiters-mode)
 (global-linum-mode)
 (projectile-global-mode)
-(ido-mode t)
-(ido-vertical-mode t)
-(ido-everywhere t)
-(flx-ido-mode t)
+(electric-indent-mode t)
+(ido-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
 ;; disable ido faces to see flx highlights.
 (setq ido-use-faces nil)
-(setq ido-enable-flex-matching t
-      flx-ido-use t
-      flx-ido-threshhold 1000
-      ido-auto-merge-work-directories-length -1)
