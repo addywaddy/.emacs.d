ebba43bf26ba2cbcba0dfdc1bd249786a7b32c55
Adam Groves <adam.groves@gmail.com>
2014-02-13 21:13:28 +0100

    Theme change

--- look.el
+++ look.el
@@ -3,8 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#110F13" :foreground "#F4EAD5" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "apple" :family "Monaco"))))
- '(linum ((t (:inherit (shadow default) :background "#111013" :foreground "#404040" :height 100))))
- '(mode-line ((t (:background "#141414" :foreground "#828282" :box nil :height 120)))))
+ '(default ((t (:inherit nil :stipple nil :background "#110F13" :foreground "#F4EAD5" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 170 :width normal :foundry "apple" :family "Inconsolata_for_Powerline"))))
+ '(linum ((t (:inherit (shadow default) :background "#111013" :foreground "#404040" :height 150))))
+ '(mode-line ((t (:background "#141414" :foreground "#828282" :box nil :height 140)))))
 
 (load-theme 'ujelly t)
